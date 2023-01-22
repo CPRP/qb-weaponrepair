a6a1ea163edcecdef3d4425de71e1a1cdebc9d71
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-01-21 19:33:02 -0500

    updated

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.Target = false -- Whether or not to use target instead of an item.
 Config.Models = { -- Put the models you would like to use here if you are using the Target option.
-    '',
+    's_m_y_ammucity_01',
 }
 Config.RemoveItem = true -- Change to false if you want Repair Kits to not be removed.
 Config.RepairTime = 60000 -- Time it takes to repair a weapon. 60000 = 60 seconds.
